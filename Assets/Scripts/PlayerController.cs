160ecb8ddae5630c12da5f95ebfee758566d4c2b
craighaworth <craig.haworth@loomitech.co.za>
2024-12-17 22:39:33 +0200

    created animator and connected to movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     public float vertical = 0f;
 
     public CameraController cameraController;
+    public Animator animatorController;
     public Quaternion targetRotation;
 
     // Update is called once per frame
@@ -18,19 +19,20 @@ public class PlayerController : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
-        float isMoving = Mathf.Abs(horizontal) + Mathf.Abs(vertical);
+        float moveAmount = Mathf.Abs(horizontal) + Mathf.Abs(vertical);
 
         moveInput = (new Vector3(horizontal, 0, vertical)).normalized;
 
         var moveDir = cameraController.planarRotation * moveInput;
         
 
-        if(isMoving > 0)
+        if(moveAmount > 0)
         {
             transform.position += moveSpeed * Time.deltaTime * moveDir;
             targetRotation = Quaternion.LookRotation(moveDir);
         }
 
         transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        animatorController.SetFloat("moveAmount", moveAmount);
     }
 }
